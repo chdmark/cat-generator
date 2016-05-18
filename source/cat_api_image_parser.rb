02f9042ecb737669bb3af3a56440510659658e18
Chris Mark <chdmark@gmail.com>
2016-05-18 16:56:13 -0700

    Add module for CatImageParser to get cat api data

---
+++ source/cat_api_image_parser.rb
@@ -0,0 +1,8 @@
+require 'httparty'
+
+module CatImageParser
+	def self.get_image
+		cat_api_data = HTTParty.get("http://thecatapi.com/api/images/get?format=xml")
+		cat_api_data["response"]["data"]["images"]["image"]["url"] 
+	end
+end
