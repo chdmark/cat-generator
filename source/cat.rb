06867e99f45c8e4809cc558051529d9af85edee3
Chris Mark <chdmark@gmail.com>
2016-05-18 17:29:42 -0700

    Add all attributes for cat in model. Passed those tests.

--- source/cat.rb
+++ source/cat.rb
@@ -1,5 +1,19 @@
 #model for the cat class. It will have its attributes and abilities here
 
 class Cat
+	#read and modify lives and alive 
+	attr_accessor :lives, :alive
+	#read cat attributes to display information
+	attr_reader :name, :color, :friendly, :image_url, :age
+
+	def initialize(args = {})
+		@name = args[:name]
+		@age = args[:age]
+		@color = args[:color]
+		@friendly = args[:friendly]
+		@image_url = args[:image_url]
+		@lives = args[:lives] || 9
+		@alive = args[:alive] || true
+	end
 
 end
