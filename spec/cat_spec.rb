a99089d98340a5e359298407a0295e3cf10b24d9
Chris Mark <chdmark@gmail.com>
2016-05-18 17:03:47 -0700

    Add test for name attribute.

--- spec/cat_spec.rb
+++ spec/cat_spec.rb
@@ -3,5 +3,13 @@ require_relative '../source/cat_api_image_parser'
 
 describe Cat do 
 
+	@cat_image = CatImageParser.get_image
+	let(:cat){ Cat.new(name: "Chris", color: "blue", age: 15, friendly: false, image_url: @cat_image)}
+	let(:dead_cat){ Cat.new(name: 'dead cat', color: 'black', age: 20, friendly: true, image_url: @cat_image, lives: 1)}
 
+	describe "attributes" do 
+		it 'has a name' do 
+			expect(cat.name).to eq 'Chris'
+		end
+	end
 end
