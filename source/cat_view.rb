2ee699c4a8387eb699af70d79679b39c57832a6c
Chris Mark <chdmark@gmail.com>
2016-05-18 22:29:39 -0700

    Add CatView#gather_response and CatView#cat_actions

--- source/cat_view.rb
+++ source/cat_view.rb
@@ -23,5 +23,38 @@ class CatView
 		 end
 	end
 
+	#method for user to select various cat actions
+	def self.cat_actions(cat)
+		input = self.gather_response
+		while input != "quit" 
+
+			if input == "1"
+				puts @random_cat.meow
+			elsif input =="2"
+				puts "What would you like it to eat?"
+				input_food = self.gather_response
+				puts cat.eat(input_food)
+			elsif input =="3"
+				puts cat.scratch
+			elsif input =="4"
+				puts cat.sleep
+			elsif input =="5"
+				puts "Would you like your cat to jump, roll, hide, duck, or attack?"
+				input_trick = self.gather_response
+				puts cat.perform_trick(input_trick)
+			else
+				puts "Input of '#{input}' is not recognized."
+				puts "Please enter a number for one of the options above!"
+			end
+
+			self.display_actions
+			input = self.gather_response
+		end
+	end
+
+	#method to gather user response or input
+	def self.gather_response
+		gets.chomp.downcase
+	end
 
 end
